4c3784b183b4a83b60e7f9dbc0c8cf337ec5d9c5
amitleiba <amitleiba012@gmail.com>
2022-12-23 17:15:32 +0200

    added all classes

---
+++ src/Main.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+#include<pjsua2.hpp>
+
+#include<boost/asio.hpp>
+
+
+int main(){
+    std::cout<<"Hi" << std::endl;
+
+    pj::Endpoint ep;
+
+    boost::asio::io_context i;
+}

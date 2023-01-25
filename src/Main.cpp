c55755309867770d5c67e37a102135f3b906b322
amitleiba <amitleiba012@gmail.com>
2023-01-25 22:47:22 +0200

    Starting the server

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,9 +1,6 @@
-#include<iostream>
-#include<string>
-
-#include"tester/ssp/SoftphoneManager.hpp"
 #include"tester/vtcp/VTCPServer.hpp"
 
 int main(){
     VTCPServer server(5060);
+    server.start();
 }
